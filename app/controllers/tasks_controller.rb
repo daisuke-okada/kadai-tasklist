4bc0f4acb43a1cb03a3b9c6a5a1318dfc23a2db7
daisuke.okada <okada.daisuke@lmi.ne.jp>
2020-06-11 01:02:43 +0000

    delete

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,7 +1,7 @@
 class TasksController < ApplicationController
 	before_action :require_user_logged_in 
 	before_action :correct_user, only: [:destroy]
-	before_action :set_task, only: [:show, :edit, :update, :destroy]
+	before_action :set_task, only: [:show, :edit, :update]
 	
 	
 	def index
@@ -42,15 +42,14 @@ class TasksController < ApplicationController
 	end
 	def destroy
 		@task.destroy
-		
 		flash[:success] = "Task は正常に削除されました"
-		redirect_to tasks_url
+		redirect_to root_path
 	end 
 	
 	private
 	
 	def set_task
-		@task = current_user.tasks.find(params[:id])
+		@task = current_user.tasks.find_by(id: params[:id])
 		
 	end
 	
@@ -59,5 +58,12 @@ class TasksController < ApplicationController
 		
 	end
 	
+	def correct_user
+		@task = current_user.tasks.find_by(id: params[:id])
+		unless @task 
+			redirect_to root_url
+		end
+		
+	end 
 	
 end
